438ab9b667bf0d682879a6094bcdae0c47c78407
behbod <behbod7899@gmail.com>
2026-05-04 14:57:15 +0330

    add build output

---
+++ out/_next/static/chunks/06604fe619edbb35.js
@@ -0,0 +1,5 @@
+__turbopack_load_page_chunks__("/_app", [
+  "static/chunks/e60ef129113f6e24.js",
+  "static/chunks/c5380a0e1704ea5e.js",
+  "static/chunks/turbopack-e2fcb297c377e860.js"
+])
---
+++ out/_next/static/chunks/5088514e80939b9b.js
@@ -0,0 +1,5 @@
+__turbopack_load_page_chunks__("/_error", [
+  "static/chunks/17722e3ac4e00587.js",
+  "static/chunks/c5380a0e1704ea5e.js",
+  "static/chunks/turbopack-767235f6ffbcaa4a.js"
+])
